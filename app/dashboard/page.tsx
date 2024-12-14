527843c2246f5b433717456eceb16f802ec9b1eb
MustafaAhmadi1994 <aminazad455@gmail.com>
2024-12-14 20:28:15 +0330

    fix bugs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,8 @@
 import Row from "@/_components/ui/Row";
 import BookingTableOperations from "./_components/BookingTableOperations";
 import BookingTable from "./_components/BookingTable";
-import { getCurrentUser } from "@/_lib/actions/user.actions";
 
 async function Dashboard() {
-  const user = await getCurrentUser();
-  const isAuthenticated = user?.role === "authenticated";
-
-  if (!isAuthenticated) return;
-
   return (
     <div className=" h-dvh pt-12 -mt-4 bg-gray-200 dark:bg-gray-800 dark:text-slate-200">
       <Row variant="horizontal">
